6182a6d63a317fc7ad08c8727b64a77813c341d5
KonstantinosNikopoulos <nikopoulo@csd.auth.gr>
2020-12-07 00:10:44 +0200

    first page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Jumbotron from "react-bootstrap/Jumbotron";
-
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import './Dashboard.css'
 
 const Navigation = () => {
     return (
@@ -28,7 +33,53 @@ const Welcome = () => {
     )
 }
 
+const Stats = () => {
+    return (
+      <React.Fragment>
+        <Container>
+            <Row>
+                <Col><div className="stat">STUDENTS: 1537</div></Col>
+                <Col><div className="stat">COURSES: 24</div></Col>
+                <Col><div className="stat">INSTRUCTORS: 36</div></Col>
+                <Col><div className="stat">EVENTS: 158</div></Col>
+            </Row>
+        </Container>
+      </React.Fragment>
+    );      
+}
 
+const Last5 = () => {
+    return (
+      <React.Fragment>
+        <div className="last5">Last 5 Courses</div>
+        <Table bordered hover style={{marginBottom : "0px"}}>
+            <thead>
+                <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Bookable</th>
+                <th>Price</th>
+                <th>Date</th>
+                <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>&#8505;</td>
+                <td>Certified Scrum Master</td>
+                <td>&#10004;</td>
+                <td>1100</td>
+                <td>27/9/2019-28/9/2019</td>
+                <td><Button variant="primary">View details</Button>{' '}</td>
+                </tr>
+            </tbody>
+        </Table>
+        <div className="viewall">
+            <Button class="btn btn-primary">View All</Button>
+        </div>
+      </React.Fragment>
+    );      
+}
 
 
 function Dashboard () {
@@ -36,6 +87,8 @@ function Dashboard () {
         <React.Fragment>
             <Navigation />   
             <Welcome />
+            <Stats/>
+            <Last5/>
          </React.Fragment>
     )
 }
