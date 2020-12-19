c7ce804746034823883e1dae2742ada8971a0baa
Konstantinos Nikopoulos <35402328+KonstantinosNikopoulos@users.noreply.github.com>
2020-12-19 15:19:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//import './App.css';
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,18 +8,22 @@ import Courses from "./components/Courses";
 import Dashboard from "./components/Dashboard";
 import Error from "./components/Error";
 import CourseDetails from "./components/CourseDetails";
+import EditCourse from "./components/EditCourse";
 
 import { Route, Switch } from "react-router-dom"
+import ScrollRestoration from 'react-scroll-restoration'
 
 function App() {
   return (
     <main>
        <AppNavigation />
+       <ScrollRestoration />
        <Switch>
          <Route path="/" component={Dashboard} exact />
          <Route path="/courses" component={Courses} />
          <Route path="/addnewcourse" component={AddCourse} />
          <Route path="/coursedetails" component={CourseDetails} />
+         <Route path="/editcourse" component={EditCourse} />
          <Route component={Error} />
        </Switch>
     </main>
